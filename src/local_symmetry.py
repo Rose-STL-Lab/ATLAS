9f940463e86cf4f6848bfb1db21bdf3145904576
enigmurl <43832426+enigmurl@users.noreply.github.com>
2024-07-09 23:06:47 -0700

    Fix top tag group basis

--- src/local_symmetry.py
+++ src/local_symmetry.py
@@ -62,31 +62,22 @@ class LocalTrainer:
             # train basis
             b_losses = []
             b_reg = []
-            """
             for xx,yy in tqdm.tqdm(xxyy):
                 xp = self.basis.apply(xx)
                 model_prediction = self.predictor.run(xp)
 
-                if config.EXPERIMENT_TYPE == "toptagging":
-                    criterion = nn.CrossEntropyLoss(reduction='mean')
-                    b_loss = criterion(model_prediction, yy)
-                else:
-                    b_loss = self.basis.loss(model_prediction, yy) * config.INVARIANCE_LOSS_COEFF
+                b_loss = self.basis.loss(model_prediction, yy) * config.INVARIANCE_LOSS_COEFF
                     
                 # don't include regularization in outputs
                 b_losses.append(float(b_loss.detach().cpu()))
 
-                if config.EXPERIMENT_TYPE == "toptagging":
-                    b_loss += torch.abs(nn.CosineSimilarity(dim=2)(xp, xx).mean())
-                else:
-                    b_loss += self.basis.regularization()
+                b_loss += self.basis.regularization()
                 
                 b_reg.append(float(b_loss.detach().cpu()))
 
                 self.basis.optimizer.zero_grad()
                 b_loss.backward()
                 self.basis.optimizer.step()
-            """
             b_losses = np.mean(b_losses) if len(b_losses) else 0
             b_reg = np.mean(b_reg ) if len(b_reg ) else 0
         
