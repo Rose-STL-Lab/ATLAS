80162fba6eacf39a12cec2244f83ebfbba2f5720
enigmurl <43832426+enigmurl@users.noreply.github.com>
2024-07-16 15:42:10 -0700

    Reorganize code

--- config.py
+++ config.py
@@ -6,8 +6,12 @@ class Config:
     def __init__(self):
         parser = argparse.ArgumentParser()
         parser.add_argument('--standard_basis', default=False, action='store_true')
+        parser.add_argument('--bs', type=int, action=64)
+        parser.add_argument('--epochs', type=int, action=30)
         args = parser.parse_args()
 
         self.standard_basis = args.standard_basis
+        self.batch_size = parsers.bs
+        self.epochs = parser.epochs
         self.device = get_device()
 
--- experiment_norm.py
+++ experiment_norm.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
-from utils import get_device
-from local_symmetry import Predictor, LocalTrainer
-from group_basis import GroupBasis
-from ff_transformer import TorusFFTransformer
-from config import Config
+from ..utils import get_device
+from ..local_symmetry import Predictor, LocalTrainer
+from ..group_basis import GroupBasis
+from ..ff_transformer import TorusFFTransformer
+from ..config import Config
 
 # torus size along each dimension (i.e. 3 -> (3, 3, 3))
 MAN_DIM = 10
